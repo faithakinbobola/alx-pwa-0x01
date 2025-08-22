88ce9d79f1bf78c057fe8615e87fb4a9021b899e
faithakinbobola <faithakinbobola2@gmail.com>
2025-08-22 17:02:30 +0100

    Link the Manifest in _document.js

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,6 +1,7 @@
+import { ButtonProps } from '@/interfaces';
 import React from 'react';
  
-const Button: React.FC = () => {
+const Button: React.FC<ButtonProps> = ({ title }) => {
     return ( 
         <div>
             <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
